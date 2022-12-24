40c7cf597cea05ca7252502579e6b67e81232b3d
Daniel Arenas <danarenasb@gmail.com>
2022-12-23 21:03:07 -0600

    gha

--- db.go
+++ db.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var (
-// dbUser                 = os.Getenv("DB_USER")
-// dbPwd                  = os.Getenv("DB_PASS")
-// instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME")
-// dbName                 = os.Getenv("DB_NAME")
-// socketDir              = "/cloudsql"
-
+	dbUser                 = os.Getenv("DB_USER")
+	dbPwd                  = os.Getenv("DB_PASS")
+	instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME")
+	dbName                 = os.Getenv("DB_NAME")
+	socketDir              = "/cloudsql"
 )
 
 func dbConnection() (*gorm.DB, error) {
-	// dsn := fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, dbPwd, dbName, socketDir, instanceConnectionName)
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432"
+	dsn := fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, dbPwd, dbName, socketDir, instanceConnectionName)
+	// dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
